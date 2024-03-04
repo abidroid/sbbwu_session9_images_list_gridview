178b119ba419cf51fc614edc6ba19d2c2c3a7424
Abid <abidroid1@gmail.com>
2024-03-04 16:29:13 +0500

    asset image doen

--- lib/screens/first_screen.dart
+++ lib/screens/first_screen.dart
@@ -6,6 +6,7 @@ class FirstScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.amber,
       appBar: AppBar(
         title: const Text(
           'Session 9',
@@ -13,21 +14,32 @@ class FirstScreen extends StatelessWidget {
         ),
         backgroundColor: Colors.pink,
       ),
-      body: Column(
-        children: [
-          Image(
-            image: NetworkImage(
-              'https://avatars.githubusercontent.com/u/17814795?v=4',
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Image(
+              image: NetworkImage(
+                'https://avatars.githubusercontent.com/u/17814795?v=4',
+              ),
+              width: 200,
+              height: 200,
             ),
-            width: 200,
-            height: 200,
-          ),
-          Image.network(
-            'https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg',
-            width: 200,
-            height: 200,
-          )
-        ],
+            Image.network(
+              'https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg',
+              width: 200,
+              height: 200,
+            ),
+        
+            Image.asset('assets/images/product_details.png'),
+        
+            CircleAvatar(
+              radius: 80,
+              backgroundImage: NetworkImage('https://avatars.githubusercontent.com/u/17814795?v=4',
+                  ),
+            )
+        
+          ],
+        ),
       ),
     );
   }
